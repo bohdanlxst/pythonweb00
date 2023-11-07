c3798fad5c9dd9d1a5998860875544b85a401ac1
bohdan <bohdan120802@gmail.com>
2023-11-07 07:25:11 +0200

    2png

--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -1,5 +1,6 @@
 import socket
 from datetime import datetime
+import time
 
 # Налаштування хоста
 host = '127.0.0.1'
@@ -21,3 +22,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             received_text = data.decode('utf-8')
             print(f"Отримано о {datetime.now()}: {received_text}")
+
+            # Затримка перед відправкою відлуння
+            time.sleep(5)
+
+            # Відправлення відлуння
+            sent = 0
+            while sent < len(data):
+                sent += conn.send(data[sent:])
+                if sent == 0:
+                    break  # Роз'єднання
+
+            if sent < len(data):
+                print("Не всі дані було відправлено успішно.")
+            else:
+                print(f"Відлуння відправлено о {datetime.now()}")
+
+        # Закривання з'єднання з клієнтом
+        print(f"З'єднання з {addr} закрите.")
