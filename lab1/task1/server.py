f17bda1e3f1fa12e5f6885b98120764493df0cb1
bohdan <bohdan120802@gmail.com>
2023-11-07 22:40:53 +0200

    4png

--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from datetime import datetime
 import time
 
@@ -6,37 +7,63 @@ import time
 host = '127.0.0.1'
 port = 20300
 
-# Створення сокету
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))  # Зв'язування сокету з хостом
-    s.listen()            # Перевід сокету в режим очікування
-    print(f"Сервер на {host}:{port}")
+# Глобальна змінна для контролю роботи сервера
+run_server = True
 
-    conn, addr = s.accept()  # Очікування на підключення
-    with conn:
-        print(f"З'єднано з {addr}")
-        while True:
-            data = conn.recv(1024)  # Прийом даних від клієнта
+
+# Обробник клієнта
+def handle_client(client_socket, addr):
+    global run_server
+    print(f"З'єднання з {addr} встановлено.")
+
+    while True:
+        try:
+            data = client_socket.recv(1024)
             if not data:
                 break
 
-            received_text = data.decode('utf-8')
-            print(f"Отримано о {datetime.now()}: {received_text}")
+            # Декодування отриманого повідомлення
+            message = data.decode('utf-8')
+            print(f"Отримано ({addr}): {message} о {datetime.now()}")
 
-            # Затримка перед відправкою відлуння
+            # Якщо отримано команду вимкнення сервера
+            if message.lower() == 'shutdown':
+                print("Отримано команду вимкнення сервера.")
+                run_server = False
+                break
+
+            # Симуляція затримки
             time.sleep(5)
 
-            # Відправлення відлуння
-            sent = 0
-            while sent < len(data):
-                sent += conn.send(data[sent:])
-                if sent == 0:
-                    break  # Роз'єднання
+            # Відправлення відповіді (відлуння)
+            sent = client_socket.send(data)
+            if sent == 0:
+                raise RuntimeError("Сокетне з'єднання перервано")
 
-            if sent < len(data):
-                print("Не всі дані було відправлено успішно.")
-            else:
-                print(f"Відлуння відправлено о {datetime.now()}")
+            print(f"Відлуння відправлено до ({addr}) о {datetime.now()}")
 
-        # Закривання з'єднання з клієнтом
-        print(f"З'єднання з {addr} закрите.")
+        except socket.error as e:
+            print(f"Помилка сокета: {e}")
+            break
+
+    # Закривання з'єднання
+    client_socket.close()
+    print(f"З'єднання з {addr} закрите.")
+
+
+# Створення сокету та прослуховування вхідних підключень
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind((host, port))
+    server_socket.listen()
+    print(f"Сервер чекає на підключення на {host}:{port}...")
+
+    while run_server:
+        try:
+            client_socket, addr = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+            client_thread.start()
+        except Exception as e:
+            print(f"Виникла помилка: {e}")
+            break
+
+    server_socket.close()
